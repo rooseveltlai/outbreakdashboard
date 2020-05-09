22a93b6d5507e384534ad3cb1ed3afa8a9ad0ece
Roosevelt Lai <roosevelt@roosevelts-mbp.lan>
2020-05-09 16:45:58 -0400

    add cases

--- src/js/appController.js
+++ src/js/appController.js
@@ -2,101 +2,102 @@
  * Your application specific code will go here
  */
 define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojrouter', 'ojs/ojarraydataprovider', 'ojs/ojknockouttemplateutils', 'ojs/ojmodule-element', 'ojs/ojknockout', 'ojs/ojprogress'],
-  function(ko, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, Router, ArrayDataProvider, KnockoutTemplateUtils) {
-     function ControllerViewModel() {
-        var self = this;
+  function (ko, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, Router, ArrayDataProvider, KnockoutTemplateUtils) {
+    function ControllerViewModel () {
+      var self = this
 
-        self.KnockoutTemplateUtils = KnockoutTemplateUtils;
+      self.KnockoutTemplateUtils = KnockoutTemplateUtils
 
-        // Handle announcements sent when pages change, for Accessibility.
-        self.manner = ko.observable('polite');
-        self.message = ko.observable();
-        document.getElementById('globalBody').addEventListener('announce', announcementHandler, false);
+      // Handle announcements sent when pages change, for Accessibility.
+      self.manner = ko.observable('polite')
+      self.message = ko.observable()
+      document.getElementById('globalBody').addEventListener('announce', announcementHandler, false)
 
-        function announcementHandler(event) {
-          setTimeout(function() {
-            self.message(event.detail.message);
-            self.manner(event.detail.manner);
-          }, 200);
-        };
+      function announcementHandler (event) {
+        setTimeout(function () {
+          self.message(event.detail.message)
+          self.manner(event.detail.manner)
+        }, 200)
+      }
 
       // Media queries for repsonsive layouts
-      var smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
-      self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+      var smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY)
+      self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery)
 
-       // Router setup
-       self.router = Router.rootInstance;
-       self.router.configure({
-         'dashboard': {label: 'Dashboard', isDefault: false},
-         'trends': {label: 'Trends', isDefault: true}
-       });
-      Router.defaults['urlAdapter'] = new Router.urlParamAdapter();
+      // Router setup
+      self.router = Router.rootInstance
+      self.router.configure({
+        'cases': {label: 'Cases', isDefault: true},
+        'dashboard': {label: 'Dashboard', isDefault: false},
+        'trends': {label: 'Trends', isDefault: false}
+      })
+      Router.defaults['urlAdapter'] = new Router.urlParamAdapter()
 
       self.loadModule = function () {
         self.moduleConfig = ko.pureComputed(function () {
-          var name = self.router.moduleConfig.name();
-          var viewPath = 'views/' + name + '.html';
-          var modelPath = 'viewModels/' + name;
+          var name = self.router.moduleConfig.name()
+          var viewPath = 'views/' + name + '.html'
+          var modelPath = 'viewModels/' + name
 
           setCookie('outbreak_bookmark', name, 365)
 
           return moduleUtils.createConfig({ viewPath: viewPath,
-            viewModelPath: modelPath, params: { parentRouter: self.router } });
-        });
-      };
+          viewModelPath: modelPath, params: { parentRouter: self.router } })
+        })
+      }
 
       // Navigation setup
       var navData = [
-      {name: 'Trends', id: 'trends',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
-      {name: 'Dashboard', id: 'dashboard',
+        {name: 'Cases', id: 'cases',
+        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-library-icon-24'},
+        {name: 'Trends', id: 'trends',
+        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
+        {name: 'Dashboard', id: 'dashboard',
         iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'}
-      ];
-      self.navDataProvider = new ArrayDataProvider(navData, {keyAttributes: 'id'});
+      ]
+      self.navDataProvider = new ArrayDataProvider(navData, {keyAttributes: 'id'})
 
       // Header
       // Application Name used in Branding Area
-      self.appName = ko.observable("Corona Virus Outbreak Dashboard");
-      self.lastUpd = ko.observable();
-      self.loading = ko.observable(true);
+      self.appName = ko.observable('Corona Virus Outbreak Dashboard')
+      self.lastUpd = ko.observable()
+      self.loading = ko.observable(true)
 
       // Footer
-      function footerLink(name, id, linkTarget) {
-        this.name = name;
-        this.linkId = id;
-        this.linkTarget = linkTarget;
+      function footerLink (name, id, linkTarget) {
+        this.name = name
+        this.linkId = id
+        this.linkTarget = linkTarget
       }
-      self.footerLinks = ko.observableArray([
-      ]);
+      self.footerLinks = ko.observableArray([])
 
       let savedUrl = getCookie('outbreak_bookmark')
-       if (savedUrl && self.router.moduleConfig.name() !== savedUrl) {
-         setTimeout(function() {self.router.go(savedUrl)}, 20)
+      if (savedUrl && self.router.moduleConfig.name() !== savedUrl) {
+        setTimeout(function () {self.router.go(savedUrl)}, 20)
       }
-
     }
 
-    function setCookie(name, value, days) {
-      var expires = "";
+    function setCookie (name, value, days) {
+      var expires = ''
       if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+        var date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = '; expires=' + date.toUTCString()
       }
-      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+      document.cookie = name + '=' + (value || '') + expires + '; path=/'
     }
 
-    function getCookie(name) {
-      var nameEQ = name + "=";
-      var ca = document.cookie.split(';');
+    function getCookie (name) {
+      var nameEQ = name + '='
+      var ca = document.cookie.split(';')
       for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
       }
-      return null;
-     }
+      return null
+    }
 
-     return new ControllerViewModel();
+    return new ControllerViewModel()
   }
-);
+)
---
+++ src/js/viewModels/cases.js
@@ -0,0 +1,182 @@
+/*
+ * Your dashboard ViewModel code goes here
+ */
+define(['../appController', 'knockout', 'jquery', 'ojs/ojbootstrap', 'ojs/ojarraydataprovider', 'ojs/ojlistdataproviderview', 'ojs/ojknockout', 'ojs/ojchart', 'ojs/ojtable'],
+  function (app, ko, $, Bootstrap, ArrayDataProvider, ListDataProviderView, DateTimeConverter) {
+    function TableDataViewModel () {
+      var self = this
+      self.tableData = ko.observableArray()
+
+      // Below are a set of the ViewModel methods invoked by the oj-module component.
+      // Please reference the oj-module jsDoc for additional information.
+
+      /**
+       * Optional ViewModel method invoked after the View is inserted into the
+       * document DOM.  The application can put logic that requires the DOM being
+       * attached here.
+       * This method might be called multiple times - after the View is created
+       * and inserted into the DOM and after the View is reconnected
+       * after being disconnected.
+       */
+      self.connected = function () {
+        document.title = 'Case in US and States - Coronavirus Outbreak Dashboard'
+        loadTableData()
+      }
+
+      self.tableDataProvider = new ArrayDataProvider(self.tableData,
+        {
+          keyAttributes: 'stateName',
+          implicitSort: [{ attribute: 'deltaPositive', direction: 'descending' }]
+        }
+      )
+
+      /**
+       * Optional ViewModel method invoked after the View is disconnected from the DOM.
+       */
+      self.disconnected = function () {
+        // Implement if needed
+      }
+
+      /**
+       * Optional ViewModel method invoked after transition to the new View is complete.
+       * That includes any possible animation between the old and the new View.
+       */
+      self.transitionCompleted = function () {}
+
+      function loadTableData () {
+        app.loading(true)
+
+        let url = `https://covidtracking.com/api/v1/states/daily.json`
+        let promise1 = $.getJSON(url)
+
+        let usUrl = `https://covidtracking.com/api/v1/us/daily.json`
+        let promise2 = $.getJSON(usUrl)
+
+        $.when(promise1, promise2).then(function (dataState, dataUSA) {
+          let usa = dataUSA[0].map(i => {
+            i.state = 'USA'
+            return i
+          })
+          data = preprocessData(dataState[0].concat(usa))
+          self.tableData(data)
+          app.loading(false)
+        })
+      }
+
+      function preprocessData (data) {
+        let currentDate = data[0].date
+        app.lastUpd(dateToISO(currentDate))
+
+        let prevDate = data.find(i => i.date < currentDate).date
+        let latestDayData = data.filter(i => i.date == currentDate)
+        let priorDayData = data.filter(i => i.date == prevDate)
+
+        let priorDayMap = {}
+        priorDayData.forEach(i => priorDayMap[i.state] = i)
+
+        let dailyPositiveMap = {}
+        data.forEach(i => {
+          let series = dailyPositiveMap[i.state] || []
+          series.unshift(i.positiveIncrease)
+          dailyPositiveMap[i.state] = series
+        })
+
+        return latestDayData.map(i => {
+          let prior = priorDayMap[i.state]
+          i.deltaPositive = i.positive - prior.positive
+          i.deltaDeath = i.death - prior.death
+          i.deltaTest = i.totalTestResults - prior.totalTestResults
+          i.posTestRate = Math.round(i.deltaPositive / i.deltaTest * 1000) / 1000
+          i.posTestRateStr = (Math.round(i.deltaPositive / i.deltaTest * 1000) / 10) + '%'
+
+          i.totalPosTestRate = Math.round(i.positive / i.totalTestResults * 1000) / 1000
+          i.totalPosTestRateStr = (Math.round(i.positive / i.totalTestResults * 1000) / 10) + '%'
+
+          i.deathRate = Math.round(i.death / i.positive * 1000) / 1000
+          i.deathRateStr = (Math.round(i.death / i.positive * 1000) / 10) + '%'
+
+          i.series = dailyPositiveMap[i.state]
+          i.stateName = STATES[i.state]
+          i.spark = new ArrayDataProvider(i.series)
+
+          return i
+        }).sort((a, b) => b.deltaPositive - a.deltaPositive)
+      }
+
+      function dateToISO (date) {
+        let dateStr = date.toString()
+        return dateStr.substring(0, 4) + '-' + dateStr.substring(4, 6) + '-' + dateStr.substring(6, 8)
+      }
+    }
+
+    var STATES = {
+      'USA': 'United States',
+      'AL': 'Alabama',
+      'AK': 'Alaska',
+      'AS': 'American Samoa',
+      'AZ': 'Arizona',
+      'AR': 'Arkansas',
+      'CA': 'California',
+      'CO': 'Colorado',
+      'CT': 'Connecticut',
+      'DE': 'Delaware',
+      'DC': 'District Of Columbia',
+      'FM': 'Federated States Of Micronesia',
+      'FL': 'Florida',
+      'GA': 'Georgia',
+      'GU': 'Guam',
+      'HI': 'Hawaii',
+      'ID': 'Idaho',
+      'IL': 'Illinois',
+      'IN': 'Indiana',
+      'IA': 'Iowa',
+      'KS': 'Kansas',
+      'KY': 'Kentucky',
+      'LA': 'Louisiana',
+      'ME': 'Maine',
+      'MH': 'Marshall Islands',
+      'MD': 'Maryland',
+      'MA': 'Massachusetts',
+      'MI': 'Michigan',
+      'MN': 'Minnesota',
+      'MS': 'Mississippi',
+      'MO': 'Missouri',
+      'MT': 'Montana',
+      'NE': 'Nebraska',
+      'NV': 'Nevada',
+      'NH': 'New Hampshire',
+      'NJ': 'New Jersey',
+      'NM': 'New Mexico',
+      'NY': 'New York',
+      'NC': 'North Carolina',
+      'ND': 'North Dakota',
+      'MP': 'Northern Mariana Islands',
+      'OH': 'Ohio',
+      'OK': 'Oklahoma',
+      'OR': 'Oregon',
+      'PW': 'Palau',
+      'PA': 'Pennsylvania',
+      'PR': 'Puerto Rico',
+      'RI': 'Rhode Island',
+      'SC': 'South Carolina',
+      'SD': 'South Dakota',
+      'TN': 'Tennessee',
+      'TX': 'Texas',
+      'UT': 'Utah',
+      'VT': 'Vermont',
+      'VI': 'Virgin Islands',
+      'VA': 'Virginia',
+      'WA': 'Washington',
+      'WV': 'West Virginia',
+      'WI': 'Wisconsin',
+      'WY': 'Wyoming'
+    }
+
+    /*
+     * Returns an instance of the ViewModel providing one instance of the ViewModel. If needed,
+     * return a constructor for the ViewModel so that the ViewModel is constructed
+     * each time the view is displayed.
+     */
+    return TableDataViewModel
+  }
+)
